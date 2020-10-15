eb0f39875c509d077522bde65dbde5c8c9e28cbd
Opilane TTHK <opilane@tthk.ee>
2020-10-15 11:35:20 +0300

    fist commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string somestring = "interpolation";
+            Console.WriteLine($"{somestring.Length}");
+            string newstring = somestring.Substring(0, 5);
+            Console.WriteLine(newstring);
+            string polationstring = somestring.Substring(5, 8);
+            Console.WriteLine(polationstring);
+        }
+    }
+}
